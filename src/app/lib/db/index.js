04540f4a38661b51b608fab31f0dd3165ca1b990
absol <rizwan.amir121@gmail.com>
2023-12-07 22:30:26 +0500

    with vercel

--- src/app/lib/db/index.js
+++ src/app/lib/db/index.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.PGSQL_USER,
-    password: process.env.PGSQL_PASSWORD,
-    host: process.env.PGSQL_HOST,
-    port: process.env.PGSQL_PORT,
-    database: process.env.PGSQL_DATABASE,
+    //user: process.env.PGSQL_USER,
+    //password: process.env.PGSQL_PASSWORD,
+    //host: process.env.PGSQL_HOST,
+    //port: process.env.PGSQL_PORT,
+    //database: process.env.PGSQL_DATABASE,
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool;
