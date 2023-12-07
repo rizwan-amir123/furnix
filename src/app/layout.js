7578558f49386f2efce7b4f7bd9b9d41d2c85fd2
absol <rizwan.amir121@gmail.com>
2023-12-08 02:16:56 +0500

    postgres changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,8 +15,7 @@ export default function RootLayout({ children }) {
     
     <html lang="en">
       <head>
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+      
       </head>
       <body className={inter.className}>
       <ClientProvider >
