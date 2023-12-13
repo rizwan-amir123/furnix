de581e8e3026dd9af1130e083b9b1299f5eed36d
absol <rizwan.amir121@gmail.com>
2023-12-14 01:27:53 +0500

    added info table

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -5,5 +5,16 @@ export async function GET(req, { params }) {
   const result = await client.query("SELECT * FROM products WHERE id = $1", [id]);
   const products = result.rows;
   client.release();
-  return Response.json(products);
+  //return Response.json(products);
+
+  return new Response(JSON.stringify(products), {
+    status: 200,
+    headers: {
+      //'Access-Control-Allow-Credentials': "true",
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      "Content-Type": "application/json"
+    },
+  });
 }
